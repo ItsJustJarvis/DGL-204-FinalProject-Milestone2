df84b5cf8c79b526257a8eee5bfef62273cd6e78
Reeve Jarvis <rjarvis@northislandcollege.ca>
2021-12-05 13:41:00 -0800

    Create player class object

--- DGL204-FinalProject-Milestone2.playground/Contents.swift
+++ DGL204-FinalProject-Milestone2.playground/Contents.swift
@@ -5,4 +5,15 @@ import Foundation
 let MAX_ROLLS = 3;
 let NUM_OF_DIE = 5;
 
+class Player {
+    let name: String
+    var score: Int
+    var numRolls: Int
+    
+    init(name:  String){
+        self.name = name
+        self.score = 0
+        self.numRolls = 0
+    }
+}
 
