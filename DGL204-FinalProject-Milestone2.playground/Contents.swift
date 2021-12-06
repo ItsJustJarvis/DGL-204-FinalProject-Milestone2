bd2ee6a3ddd08be5b580eb4c00dd8c712669d238
Reeve Jarvis <rjarvis@northislandcollege.ca>
2021-12-05 22:08:24 -0800

    Create logic to track face-value scorecard items

--- DGL204-FinalProject-Milestone2.playground/Contents.swift
+++ DGL204-FinalProject-Milestone2.playground/Contents.swift
@@ -113,11 +113,46 @@ struct Scorecard {
         var sum = 0
         switch item {
         case .ones:
+            for die in hand.dice{
+                sum += (die.value == 1) ? 1 : 0
+            }
+            values[.ones] = sum
+            trackedItems += 1
+            
         case .twos:
+            for die in hand.dice{
+                sum += (die.value == 2) ? 2 : 0
+            }
+            values[.twos] = sum
+            trackedItems += 1
+            
         case .threes:
+            for die in hand.dice{
+                sum += (die.value == 3) ? 3 : 0
+            }
+            values[.threes] = sum
+            trackedItems += 1
+            
         case .fours:
+            for die in hand.dice{
+                sum += (die.value == 4) ? 4 : 0
+            }
+            values[.fours] = sum
+            trackedItems += 1
         case .fives:
+            for die in hand.dice{
+                sum += (die.value == 5) ? 5 : 0
+            }
+            values[.fives] = sum
+            trackedItems += 1
+            
         case .sixes:
+            for die in hand.dice{
+                sum += (die.value == 6) ? 6 : 0
+            }
+            values[.sixes] = sum
+            trackedItems += 1
+            
         case .threeOfKind:
         case .fourOfKind:
         case .fullHouse:
